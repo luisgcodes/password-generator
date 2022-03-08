a80782bb1d2206fef53f5511fd32fef4021a4094
luisgcodes <lcienfuegos95@gmail.com>
2022-03-08 15:59:44 -0800

    added variables

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,28 @@
-// Assignment code here
+// Special characters, letters and numbers
+var chars = "!@#$%^&*()-_=+<>{}[]abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+
+// password criteria
+function passwordCriteria() {
+  inputLength = window.prompt("Choose length of characters between 8 & 128")
+    console.log(passwordLength);
+
+    if (passwordLength < 8 || passwordLength > 128) {
+      window.alert("Pick a valid number");
+  }
+}
+
+// click for lowercase, uppercase, numbers and special characters
+    var lowerCase = window.confirm ("Do you want lower case characters in your password?")
+    var upperCase = window.confirm ("Do you want upper case characters in your password?")
+    var numeric = window.confirm ("Do you want numbers in your password?")
+    var SpecialChars = window.confirm ("Do you want special characters in your password?")
+    
+
 
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
 
 // Write password to the #password input
 function writePassword() {
