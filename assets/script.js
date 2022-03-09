01121d51e0bea9216a211c2bdbbac25def8718f8
luisgcodes <lcienfuegos95@gmail.com>
2022-03-08 17:55:03 -0800

    turned chars into arrays/added userInput

--- assets/script.js
+++ assets/script.js
@@ -1,24 +1,57 @@
 // Special characters, letters and numbers
-var chars = "!@#$%^&*()-_=+<>{}[]abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+var lowerC = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+var upperC = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+var specialC = ["!", "@", "#", "$", "%", "^", "&", "*", "()", "-", "_", "=", "+", "<", ">", "/", "|", "?", "{", "}", "[", "]"];
 
 // password criteria
 function passwordCriteria() {
-  inputLength = window.prompt("Choose length of characters between 8 & 128")
-    console.log(passwordLength);
+  var inputLength = parseInt(window.prompt("Choose length of characters between 8 & 128"))
+    console.log(inputLength);
 
-    if (passwordLength < 8 || passwordLength > 128) {
+    if (inputLength < 8 || inputLength > 128) {
       window.alert("Pick a valid number");
+
+      passwordCriteria();
+    }
+
+// click for desired criteria
+    var lowerCase = window.confirm ("Do you want lower case characters in your password?")
+
+    var upperCase = window.confirm ("Do you want upper case characters in your password?")
+
+    var numeric = window.confirm ("Do you want numbers in your password?")
+
+    var specialChars = window.confirm ("Do you want special characters in your password?")
+    
+if (lowerCase || upperCase || numeric || specialChars) {
+  window.alert("Please select a character type to meet criteria")
+
+  passwordCriteria()
+  // return passwordCriteria;
+return;
+}
+else{
+  var userInput = {
+    inputLength: inputLength,
+    lowerCase: lowerCase,
+    upperCase: upperCase,
+    numeric: numeric,
+    specialC: specialC,
   }
 }
+return userInput
+}
 
-// click for lowercase, uppercase, numbers and special characters
-    var lowerCase = window.confirm ("Do you want lower case characters in your password?")
-    var upperCase = window.confirm ("Do you want upper case characters in your password?")
-    var numeric = window.confirm ("Do you want numbers in your password?")
-    var SpecialChars = window.confirm ("Do you want special characters in your password?")
-    
+function selectRandom (array) {
+  var randomIndex = Math.floor(Math.random() * array.length)
+  var randomEl = array[randomIndex]
 
-
+  return randomEl;
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
