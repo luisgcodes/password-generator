7300c1a69147cc741ee603b47f6a7ff1d1a001d9
luisgcodes <lcienfuegos95@gmail.com>
2022-03-10 19:41:26 -0800

    restructured

--- assets/script.js
+++ assets/script.js
@@ -5,53 +5,39 @@ var upperC = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "
 
 var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-var specialC = ["!", "@", "#", "$", "%", "^", "&", "*", "()", "-", "_", "=", "+", "<", ">", "/", "|", "?", "{", "}", "[", "]"];
+var specialC = ["!", "@", "#", "$", "%", "&", "*", "()", "-", "_", "=", "+", "<", ">", "/", "|", "~", "`", "?", "{", "}", "[", "]"];
 
 // password criteria
-function passwordCriteria() {
+function generatePassword() {
+  var passwordCriteria
   var inputLength = parseInt(window.prompt("Choose length of characters between 8 & 128"))
     console.log(inputLength);
 
     if (inputLength < 8 || inputLength > 128) {
       window.alert("Pick a valid number");
 
-      passwordCriteria();
+     return generatePassword();
+
     }
 
-// click for desired criteria
-    var lowerCase = window.confirm ("Do you want lower case characters in your password?")
-
-    var upperCase = window.confirm ("Do you want upper case characters in your password?")
-
-    var numeric = window.confirm ("Do you want numbers in your password?")
-
-    var specialChars = window.confirm ("Do you want special characters in your password?")
+    var lowerCase = window.confirm ("Do you want lower case characters in your password?");
+      if(lowerCase){
+        passwordCriteria=passwordCriteria.concat(lowerC);
+      }
+    var upperCase = window.confirm ("Do you want upper case characters in your password?");
+      if(upperCase){
+        passwordCriteria=passwordCriteria.concat(upperC);
+      }
+    var numeric = window.confirm ("Do you want numbers in your password?");
+      if(numeric){
+        passwordCriteria=passwordCriteria.concat(numbers);
+      }
+    var specialChars=window.confirm ("Do you want special characters in your password?");
+      if(specialChars){
+        passwordCriteria=passwordCriteria.concat(specialC);
+      }
     
-if (lowerCase || upperCase || numeric || specialChars) {
-  window.alert("Please select a character type to meet criteria")
-
-  passwordCriteria()
-  // return passwordCriteria;
-return;
-}
-else{
-  var userInput = {
-    inputLength: inputLength,
-    lowerCase: lowerCase,
-    upperCase: upperCase,
-    numeric: numeric,
-    specialC: specialC,
-  }
-}
-return userInput
-}
-
-function selectRandom (array) {
-  var randomIndex = Math.floor(Math.random() * array.length)
-  var randomEl = array[randomIndex]
-
-  return randomEl;
-}
+    
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
